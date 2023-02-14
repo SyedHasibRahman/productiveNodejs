cf446b2a7cfc75dd42de87b1b1d7d64383e29f25
SyedHasibRahman <syedhasib01@gmail.com>
2023-02-14 15:50:17 +0600

    first commit

---
+++ NestJs-CRUD-for-beginner/src/projects/dto/create-project.dto.ts
@@ -0,0 +1,6 @@
+export class CreateProjectDto {
+  id: number;
+  title: string;
+  img: string;
+  location: string;
+}
---
+++ NestJs-CRUD-for-beginner/src/projects/entities/project.entity.ts
@@ -0,0 +1,22 @@
+import { CreateProjectDto } from '../dto/create-project.dto';
+
+export const projects: CreateProjectDto[] = [
+  {
+    id: 1,
+    title: 'Villa on Washington Avenue',
+    img: 'https://i.ibb.co/MsTsNcn/Mask-Group.png',
+    location: 'Dhaka, Bangladesh',
+  },
+  {
+    id: 2,
+    title: 'Luxury villa in Rego Park',
+    img: 'https://i.ibb.co/ckn8Ps9/pierre-chatel-innocenti-u4dd-Xsj-ULQ-unsplash-1.png',
+    location: 'Dhaka, Bangladesh',
+  },
+  {
+    id: 3,
+    title: 'Gorgeous house',
+    img: 'https://i.ibb.co/r5NGXkP/Mask-Group-1.png',
+    location: 'Dhaka, Bangladesh',
+  },
+];
